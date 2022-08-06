d6e6434b200e32fc1a4658fab337e87eeabea4b6
William Arnberg <arnberg19@gmail.com>
2022-08-06 23:14:27 +0200

    finished

---
+++ Chapter-4/Try-This/4.4_Char-For-Loop/4.4_Char-For-Loop-Modified/4.4_Char-For-Loop-Modified/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using std::cout;
+using std::cin;
+using std::endl;
+int main() {
+
+	char a = 65;
+	for (int i = 0; i <= 25; i++) {
+		cout << a << "\t" << i <<  endl;
+		a++;
+	}
+
+
+}
+
